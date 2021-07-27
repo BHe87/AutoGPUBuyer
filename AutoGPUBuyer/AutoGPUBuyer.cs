542a715c75aa3eb0b87a614c774cb74db35e46c7
Brandon He <theblazesamurai@gmail.com>
2021-07-27 15:28:46 -0400

    testing github extension

---
+++ AutoGPUBuyer/AutoGPUBuyer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutoGPUBuyer
+{
+    class AutoGPUBuyer
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+        }
+    }
+}
